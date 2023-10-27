3dc97ea42b0fca2ff602793c37386505be264f3e
Juho <saho_@hotmail.fi>
2023-10-27 14:55:23 +0300

    fade effect ja yhteydtiedot

--- scripts/fade.js
+++ scripts/fade.js
@@ -1,8 +1,3 @@
-let img = document.getElementById('kuvat');
-let images = ['/images/matte1.jpg','/images/matte.jpg','/images/maisema1.jpg']
-let time = 6000
-let lastIndex = 0;
-
 var element = document.querySelector('.fade-out-element'); 
          function fadeOut(el) {
             var opacity = 1; // Initial opacity
@@ -17,7 +12,7 @@ var element = document.querySelector('.fade-out-element');
          }
          fadeOut(element);
 
-         function fadeIn(el) {
+         function fadeIN(el) {
             var opacity = 0; // Initial opacity
             var interval = setInterval(function() {
                if (opacity < 1) {
@@ -26,28 +21,7 @@ var element = document.querySelector('.fade-out-element');
                } else {
                   clearInterval(interval); // Stop the interval when opacity reaches 0
                }
-            }, 100);
+            }, 50);
          }
 
-         fadeIn(element)
-
-function changeImg(){
-    
-    let randomIndex = Math.floor(Math.random() * images.length);
-    while (lastIndex == randomIndex){
-        randomIndex = Math.floor(Math.random() * images.length);
-    }
-    lastIndex = randomIndex;
-
-    kuva = images[randomIndex];
-
-    
-    img.src = kuva;
-    img.style.opacity=0;
-    fadeIn(img);
-}
-window.onload = changeImg;
-
-setInterval(changeImg
-,time);
-
+         fadeIN(element)
